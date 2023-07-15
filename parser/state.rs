78543326317ec94dd901e0544e31a9f0107fe119
lg <lauren@informal.systems>
2023-07-16 01:39:36 +0200

    Update parser for state.rs

--- parser/state.rs
+++ parser/state.rs
@@ -13,10 +13,20 @@ pub struct Config {
 
 #[cw_serde]
 pub struct Lotto {
+	pub nonce: u32,
 	pub deposit: Coin,
 	pub balance: Uint128,
 	pub depositors: Vec<Addr>,
 	pub expiration: Timestamp,
-	pub o_winner: Addr,
-	pub nonce: u32,
+	pub winner: Option<Addr>,
 }
+
+pub const CONFIG_KEY: &str = "config";
+pub const NOIS_PROXY_KEY: &str = "nois_proxy";
+pub const MIN_DEP_KEY: &str = "min_dep";
+pub const LOTTOS_KEY: &str = "lottos";
+
+pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
+pub const NOIS_PROXY: Item<Addr> = Item::new(NOIS_PROXY_KEY);
+pub const MIN_DEP: Item<Coin> = Item::new(MIN_DEP_KEY);
+pub const LOTTOS: Map<u32:Lotto> = Map::new(LOTTOS_KEY);
