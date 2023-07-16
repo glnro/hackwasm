24e985de17e2f76146c02e831e63f65e30c5bf2b
lg <lauren@informal.systems>
2023-07-16 03:11:44 +0200

    Fix

--- parser/state.rs
+++ parser/state.rs
@@ -5,22 +5,22 @@ use cw_storage_plus::{Item, Map};
 #[cw_serde]
 pub struct Config {
 	pub manager: Addr,
-	pub lotto_nonce: Option<u32>,
+	pub lotto_nonce: u32,
 	pub nois_proxy: Addr,
 }
 
 #[cw_serde]
 pub struct Lotto {
+	pub deposit: Coin,
 	pub balance: Uint128,
 	pub depositors: Vec<Addr>,
 	pub expiration: Timestamp,
 	pub winner: Option<Addr>,
 	pub nonce: u32,
-	pub deposit: Coin,
 }
 
 pub const CONFIG_KEY: &str = "config";
 pub const LOTTOS_KEY: &str = "lottos";
 
-pub const CONFIG: Item<SS_Config> = Item::new(CONFIG_KEY);
+pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
 pub const LOTTOS: Map<u32,Lotto> = Map::new(LOTTOS_KEY);
