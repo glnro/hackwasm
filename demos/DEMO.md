d4de2db6ee78d0cd0da880d68f632667862cf828
lg <lauren@informal.systems>
2023-07-16 11:38:27 +0200

    Update demo

---
+++ demos/DEMO.md
@@ -0,0 +1,8 @@
+## Demos
+
+### Lottery Demo
+[Video](https://www.youtube.com/watch?v=c-AlBQQgdKo)
+
+### Quint Spec -> Cosmwasm Contract Conversion Demo
+
+### Quint Model Demo
--- lotto/src/state.rs
+++ lotto/src/state.rs
@@ -2,26 +2,25 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Coin, Timestamp, Uint128};
 use cw_storage_plus::{Item, Map};
 
-// Initialize a contract with the admin address and lotto id generator nonce
 #[cw_serde]
 pub struct Config {
-    pub manager: Addr,
-    pub lotto_nonce: u32,
-    pub nois_proxy: Addr,
+	pub nois_proxy: Addr,
+	pub manager: Addr,
+	pub lotto_nonce: u32,
 }
 
 #[cw_serde]
 pub struct Lotto {
-    pub nonce: u32,
-    pub deposit: Coin,
-    pub balance: Uint128,
-    pub depositors: Vec<Addr>,
-    pub expiration: Timestamp, // how to set expiration
-    pub winner: Option<Addr>,
+	pub deposit: Coin,
+	pub balance: Uint128,
+	pub depositors: Vec<Addr>,
+	pub expiration: Timestamp,
+	pub winner: Option<Addr>,
+	pub nonce: u32,
 }
 
 pub const CONFIG_KEY: &str = "config";
 pub const LOTTOS_KEY: &str = "lottos";
 
 pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
-pub const LOTTOS: Map<u32, Lotto> = Map::new(LOTTOS_KEY);
+pub const LOTTOS: Map<u32,Lotto> = Map::new(LOTTOS_KEY);
